9f04e94296bbf43eafe38fab65536bf304fa132d
Hiromu Miyagi <Keytalk0808@gmail.com>
2020-12-12 16:24:19 +0900

    続きを見る

--- static/js/common.js
+++ static/js/common.js
@@ -4,3 +4,10 @@ $(document).ready(function () {
         pasue: 5000,
     });
 });
+
+$(function () {
+    $(".readmore").on("click", function () {
+        $(this).toggleClass("on-click");
+        $(".hide-text").slideToggle(1000);
+    });
+});
