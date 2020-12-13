20b826be61d2f767747ac5094d0330599b3f931c
tatsuya irita <aira.fcllabo@gmail.com>
2020-12-14 01:12:51 +0900

    動作確認

--- aira.py
+++ aira.py
@@ -8,38 +8,34 @@ app.secret_key = 'sunabaco'
 
 
 
-@app.route("/new", methods=["GET"])
+@app.route("/bb", methods=["GET"])
 def new_get():
-    if "user_id" in session:
-        return redirect("/main")
-    else:     
-        return render_template("new.html")
+       
+    return render_template("bb.html")
 
 
 
-@app.route("/new", methods=["POST"])
+@app.route("/bb", methods=["POST"])
 def new_post():
-    if "user_id" in session:
-        return redirect("/main")
-    else:    
-        name = request.form.get("users_name")
-        email = request.form.get("users_email")
-        password = request.form.get("users_password")
-        representative = request.form.get("users_representative")
-        local = request.form.get("users_local")
-        introduce = request.form.get("users_introduce")
-        usimage = request.form.get("users_image")
-        #flasktest.db接続
-        conn = sqlite3.connect("20201209.db")
-        #中を見れるようにする
-        c = conn.cursor()
-        #sqlを実行
-        c.execute("insert into users values(null,?,?,?,?,?,?,?)",( name, email, password, representative, local, introduce, users_image ))
-        #保存する
-        conn.commit()
-        #データベース読み込み終了
-        c.close()
-        return "登録完了"
+   
+    name = request.form.get("users_name")
+    email = request.form.get("users_email")
+    password = request.form.get("users_password")
+    representative = request.form.get("users_representative")
+    local = request.form.get("users_local")
+    introduce = request.form.get("users_introduce")
+    image = request.form.get("users_image")
+    #flasktest.db接続
+    conn = sqlite3.connect("20201209.db")
+    #中を見れるようにする
+    c = conn.cursor()
+    #sqlを実行
+    c.execute("insert into users values(null,?,?,?,?,?,?,?)",( name, email, password, representative, local, introduce, image ))
+    #保存する
+    conn.commit()
+    #データベース読み込み終了
+    c.close()
+    return "登録完了"
 
 
 if __name__ == "__main__":
