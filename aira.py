7014f8f9378b8e1de8d5141d5d3fce13eb411444
tatsuya irita <aira.fcllabo@gmail.com>
2020-12-13 11:13:02 +0900

    更新

--- aira.py
+++ aira.py
@@ -3,9 +3,23 @@ import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/new")
+# Flask では標準で Flask.secret_key を設定すると、sessionを使うことができます。この時、Flask では session の内容を署名付きで Cookie に保存します。
+app.secret_key = 'sunabaco'
 
-def 
+@app.route("/new")
+def new():
+    #flasktest.db接続
+    conn = sqlite3.connect("20201209.db")
+    #中を見れるようにする
+    c = conn.cursor()
+    #sqlを実行
+    c.execute("init name, email, address from users where id = 1")
+    #変数にSQLで取得した内容を格納する
+    user_info = c.fetchone()
+    #データベース読み込み終了
+    c.close()
+
+    return render_template("new.html",)
 
 
 
