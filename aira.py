7f8e64ac24dde1cd5b0d4f0101472ec88d1ce782
tatsuya irita <aira.fcllabo@gmail.com>
2020-12-13 09:36:51 +0900

    ｐｙてょん書き始めます

---
+++ aira.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, session
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/new")
+
+def 
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+# ここより下に書いても実行されないので注意
