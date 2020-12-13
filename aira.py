65a340ec9403b224456b7e3dc0d4f8feca102185
tatsuya irita <aira.fcllabo@gmail.com>
2020-12-13 15:31:23 +0900

    python動くかな

--- aira.py
+++ aira.py
@@ -3,32 +3,47 @@ import sqlite3
 
 app = Flask(__name__)
 
-# Flask では標準で Flask.secret_key を設定すると、sessionを使うことができます。この時、Flask では session の内容を署名付きで Cookie に保存します。
+#秘密のカギ secret_keyの設定 sessionを使えるようにするために必要です
 app.secret_key = 'sunabaco'
 
-@app.route("/new")
-def new():
-    #flasktest.db接続
-    conn = sqlite3.connect("20201209.db")
-    #中を見れるようにする
-    c = conn.cursor()
-    #sqlを実行
-    c.execute("init name, email, address from users where id = 1")
-    #変数にSQLで取得した内容を格納する
-    user_info = c.fetchone()
-    #データベース読み込み終了
-    c.close()
 
-    return render_template("new.html",)
+
+@app.route("/new", methods=["GET"])
+def new_get():
+    if "user_id" in session:
+        return redirect("/main")
+    else:     
+        return render_template("new.html")
 
 
 
+@app.route("/new", methods=["POST"])
+def new_post():
+    if "user_id" in session:
+        return redirect("/main")
+    else:    
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        representative = request.form.get("representative")
+        local = request.form.get("local")
+        introduce = request.form.get("introduce")
+        image = request.form.get("image")
+        #flasktest.db接続
+        conn = sqlite3.connect("20201209.db")
+        #中を見れるようにする
+        c = conn.cursor()
+        #sqlを実行
+        c.execute("insert into users values(null,?,?,?,?,?,?,?)",( name, email, password, representative, local, introduce, image ))
+        #保存する
+        conn.commit()
+        #データベース読み込み終了
+        c.close()
 
 
 
+if __name__ == "__main__":
+    app.run(debug=True) 
 
-
-if __name__=="__main__":
-    app.run(debug=True)
 
 # ここより下に書いても実行されないので注意
